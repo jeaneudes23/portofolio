3248e0a8f062bad5dac8749e9d702db283879d62
Nihangaza Jean Eudes <121790290+jeaneudes23@users.noreply.github.com>
2025-05-18 00:14:38 +0200

    dynamic admin page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,8 @@
 import React, { PropsWithChildren } from "react";
 import { Toaster } from "react-hot-toast";
 
+export const revalidate = 0;
+
 export default function AdminLayout({ children }: PropsWithChildren) {
   return (
     <>
